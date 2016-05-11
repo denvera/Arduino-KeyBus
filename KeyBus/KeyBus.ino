4c5ceeecd3bea91e6a319a48fa7ddf7c8aa67173
Denver A <denvera@gmail.com>
2016-05-11 23:40:07 +0200

    Read Stable

--- KeyBus/KeyBus.ino
+++ KeyBus/KeyBus.ino
@@ -3,14 +3,18 @@
 
 #define STATUS_LED 13 // PC7
 
+#define MASTER 0
+#define CLIENT 1
+
 volatile unsigned long read_ts = 0;
 volatile unsigned long led_ts = 0;
 
-volatile unsigned char cur_msg[32];
-volatile unsigned char cur_c_msg[32];
+volatile unsigned char cur_msg[2][32];
+volatile unsigned char last_msg[2][32];
+
 volatile char byte_count = 0;
 volatile char bit_count = 0;
-volatile bool new_bit = false, bit_processed = false;
+volatile bool new_bit = false, bit_processed = false, write_msg = false;
 
 enum state { IDLE, NEW_BIT, WAIT_READ };
 enum state current_state = IDLE;
@@ -28,34 +32,37 @@ void setup() {
   Serial1.begin(115200);
   Serial1.println("DSC KeyBus Gateway 0.1\r\n");
   // Interrupt 1 = Pin 2 = PD1
-  attachInterrupt(1, bit_isr, RISING);
-  memset((char *)cur_msg, 0, 32);
+  attachInterrupt(1, bit_isr, CHANGE);
+  memset((char *)cur_msg[MASTER], 0, 32);
+  memset((char *)cur_msg[CLIENT], 0, 32);
 }
 
 void loop() {
-  if (millis() - led_ts > 1000) {
+  /*if (millis() - led_ts > 1000) {
     PORTC ^= 1 << 7;
     led_ts = millis();
-  }  
+  } */ 
   if (new_bit) {
     read_ts = micros();
+    ++bit_count;
+    if (bit_count == 8 || bit_count == 9 || (bit_count > 9 && ((bit_count - 9) % 8 == 0))) byte_count++;    
+    //delayMicroseconds(660);
+    cur_msg[CLIENT][byte_count] <<= 1;
+    cur_msg[CLIENT][byte_count] |= ((PIND >> 0) & 1);
     new_bit = false;
     bit_processed = false;
-    ++bit_count;
-    if (bit_count == 8 || bit_count == 9 || (bit_count > 9 && ((bit_count - 9) % 8 == 0))) byte_count++;
-  }
-  if ((micros() - read_ts > 600) && !bit_processed) {
+    
+    
+  //}
+  //if ((micros() - read_ts > 50) && !bit_processed) {
     //new_bit = false;        
     // Read bit from client
-    cur_c_msg[byte_count] <<= 1;
-    cur_c_msg[byte_count] |= ((PIND >> 0) & 1);
-    // Write if necessary
-    bit_processed = true;
-    // Split up bytes
 
-    current_state = IDLE;
+    // Write if necessary
+    //bit_processed = true;
+    // Split up bytes    
   }
-  if (((micros() - read_ts) > 2000) && (bit_count >= 17)) {
+  if (((micros() - read_ts) > 4000) && (bit_count >= 17)) {
     new_msg();
   }  
 }
@@ -64,8 +71,8 @@ void bit_isr() {
   if (PIND & _BV(1)) {
     // Rising, read
     //read_ts = micros();
-    cur_msg[byte_count] <<= 1;
-    cur_msg[byte_count] |= (PIND >> 0) & 1;
+    cur_msg[MASTER][byte_count] <<= 1;
+    cur_msg[MASTER][byte_count] |= (PIND >> 0) & 1;
     //cur_msg[byte_count] |= digitalRead(DSC_DATA);
     new_bit = true;    
   } else {
@@ -81,16 +88,16 @@ void new_msg() {
     bit_count = 0;  
     Serial1.print("]: ");
     for (int i = 0; i < byte_count; i++) {
-      Serial1.print((unsigned char)cur_msg[i], HEX); Serial1.print(" ");
+      Serial1.print((unsigned char)cur_msg[MASTER][i], HEX); Serial1.print(" ");
     }
     Serial1.print("\tC: ");
-    for (int i = 0; i < byte_count; i++) {
-      Serial1.print((unsigned char)cur_c_msg[i], HEX); Serial1.print(" ");
-    }
+    /*for (int i = 0; i < byte_count; i++) {
+      Serial1.print((unsigned char)cur_msg[CLIENT][i], HEX); Serial1.print(" ");
+    }*/
     byte_count = 0;
     Serial1.println();
-    memset((char *)cur_msg, 0, 32);
-    memset((char *)cur_c_msg, 0, 32);
+    memset((char *)cur_msg[MASTER], 0, 32);
+    memset((char *)cur_msg[CLIENT], 0, 32);
   }
 }
 
